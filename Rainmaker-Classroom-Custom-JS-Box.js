d9af51ad90d5fb3353030a280e324c02600cc673
Charlie Sattgast <cwsattgast@gmail.com>
2023-09-11 11:27:49 -0700

    Update Rainmaker-Classroom-Custom-JS-Box.js
    Updates code to watch for URL change without full page refresh

--- Rainmaker-Classroom-Custom-JS-Box.js
+++ Rainmaker-Classroom-Custom-JS-Box.js
@@ -1,27 +1,54 @@
-/*Code for RainmakerAI membership product Custom JS Box
-*******************************************
-*/
-
 /*
- * Script checks every 1.5 seconds whether lesson text is truncated with 'Read more' button to expand,
+ * On initial page load, and on every lesson navigation (which results in URL change, but no full page refresh):
+ * Script checks every 0.5 seconds whether lesson text is truncated with 'Read more' button to expand,
  * and generates a click on the 'Read more' button if detected, so user sees full text almost immediately.
- * Interval is not cleared immediately because navigating between lessons does not result in full document reload.
+ * Each new interval is cleared after click is generated on 'Read more' button, or 5 seconds, whichever is sooner.
+ *
+ * MutationObserver implementation for detecting URL change based on:
+ * https://stackoverflow.com/questions/3522090/event-when-window-location-href-changes
 */
 (function() {
   // `intervalDuration` and `maxLife` are configured in seconds
-  var intervalDuration = 1.5;
-  var maxLife = 7200; // cancel interval after 2 hours
+  var intervalDuration = 0.5;
+  var maxLife = 5; // cancel interval after 5 seconds
   var maxCount = maxLife / intervalDuration;
-  var count = 0;
 
-  var expandLesson = setInterval(function() {
-    if (count > maxCount) {
-      clearInterval(expandLesson);
+  // Interval that is run on initial page load
+  var countOuter = 0;
+  var expandLessonOnPageLoad = setInterval(function() {
+    if (countOuter > maxCount) {
+      clearInterval(expandLessonOnPageLoad);
     }
     var readMoreEl = document.getElementById('read-more-post');
     if (readMoreEl) {
       readMoreEl.click();
+      clearInterval(expandLessonOnPageLoad);
     }
-    count++;
+    countOuter++;
   }, intervalDuration * 1000);
+
+  // Listen for an HREF change, such as a user navigating to a different lesson without refreshing page
+  var currentHref = document.location.href;
+  var bodyElement = document.querySelector('body');
+  var observer = new MutationObserver(function(mutations) {
+    if (currentHref !== document.location.href) {
+      currentHref = document.location.href;
+
+      // Interval that is run on HREF change
+      var countInner = 0;
+      var expandLessonOnHrefChange = setInterval(function() {
+        if (countInner > maxCount) {
+          clearInterval(expandLessonOnHrefChange);
+        }
+        var readMoreEl = document.getElementById('read-more-post');
+        if (readMoreEl) {
+          readMoreEl.click();
+          clearInterval(expandLessonOnHrefChange);
+        }
+        countInner++;
+      }, intervalDuration * 1000);
+    }
+  });
+
+  observer.observe(bodyElement, { childList: true, subtree: true });
 })();
